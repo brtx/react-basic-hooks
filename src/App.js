04332610461d4edfa52d84d018b76b99d955e880
Stefan Barthel <stefan.barthel@zuehlke.com>
2020-01-12 22:10:05 +0100

    customHook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(1);
+function useCounter(initialCount) {
+  const [count, setCount] = useState(initialCount);
   useEffect(() => { document.title = `foo ${count}` });
+
+  return [count, setCount];
+}
+
+function App() {
+  const [count, setCount] = useCounter(1);
+  const [anotherCount, setAnotherCount] = useCounter(23);
 
   return (
     <div className="App">
       <div>Count {count}</div>
       <button onClick={() => setCount(count + 1)}>+1</button>
+
+      <div>Another Count {anotherCount}</div>
+      <button onClick={() => setAnotherCount(anotherCount + 10)}>+1</button>
     </div>
   );
 }
